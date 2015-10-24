635058c33e1e5f635f3454f22d29929edbdc247d
Dominic Wong <dominic.wong.617@gmail.com>
2015-10-24 22:16:45 +0800

    Storing todo lists in disk

--- lib/todo/cache.ex
+++ lib/todo/cache.ex
@@ -6,21 +6,22 @@ defmodule Todo.Cache do
   end
 
   def init(_) do
+    Todo.Database.start("./persist/")
     {:ok, HashDict.new}
   end
 
-  def server_process(cache_pid, todo_list_name) do
-    GenServer.call(cache_pid, {:server_process, todo_list_name})
+  def server_process(cache_pid, name) do
+    GenServer.call(cache_pid, {:server_process, name})
   end
 
-  def handle_call({:server_process, todo_list_name}, _, todo_servers) do
-    case HashDict.fetch(todo_servers, todo_list_name) do
+  def handle_call({:server_process, name}, _, todo_servers) do
+    case HashDict.fetch(todo_servers, name) do
       {:ok, todo_server} ->
         {:reply, todo_server, todo_servers}
 
       :error ->
-        {:ok, new_server} = Todo.Server.start
-        {:reply, new_server, HashDict.put(todo_servers, todo_list_name, new_server)}
+        {:ok, new_server} = Todo.Server.start(name)
+        {:reply, new_server, HashDict.put(todo_servers, name, new_server)}
     end
   end
 end
