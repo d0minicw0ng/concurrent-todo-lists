e7647c0aa157301107c4f22160ff41c5de0803dc
Dominic Wong <dominic.wong.617@gmail.com>
2015-10-26 14:14:15 +0800

    Supervisor

--- lib/todo/cache.ex
+++ lib/todo/cache.ex
@@ -1,17 +1,18 @@
 defmodule Todo.Cache do
   use GenServer
 
-  def start do
-    GenServer.start(__MODULE__, nil)
+  def start_link do
+    IO.puts "Starting Todo Cache."
+    GenServer.start_link(__MODULE__, nil, name: :todo_cache)
   end
 
   def init(_) do
-    Todo.Database.start("./persist/")
+    Todo.Database.start_link("./persist/")
     {:ok, HashDict.new}
   end
 
-  def server_process(cache_pid, name) do
-    GenServer.call(cache_pid, {:server_process, name})
+  def server_process(name) do
+    GenServer.call(:todo_cache, {:server_process, name})
   end
 
   def handle_call({:server_process, name}, _, todo_servers) do
@@ -20,7 +21,7 @@ defmodule Todo.Cache do
         {:reply, todo_server, todo_servers}
 
       :error ->
-        {:ok, new_server} = Todo.Server.start(name)
+        {:ok, new_server} = Todo.Server.start_link(name)
         {:reply, new_server, HashDict.put(todo_servers, name, new_server)}
     end
   end
