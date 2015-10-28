a1cae37afcd30dea8eb61ac936e12448874ee6d4
Dominic Wong <dominic.wong.617@gmail.com>
2015-10-29 00:30:09 +0800

    HTTP server

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Todo.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [
-      applications: [:gproc],
+      applications: [:gproc, :cowboy, :plug],
       mod: {Todo.Application, []}
     ]
   end
@@ -32,7 +32,10 @@ defmodule Todo.Mixfile do
   defp deps do
     [
       {:gproc, "0.3.1"},
-      {:meck, "0.8.2", only: :test}
+      {:cowboy, "1.0.0"},
+      {:plug, "0.10.0"},
+      {:meck, "0.8.2", only: :test},
+      {:httpoison, "0.4.3", only: :test}
     ]
   end
 end
